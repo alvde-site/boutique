f124a8ae5c1b005eb376a6f4ae576e714f15ac08
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-20 23:33:02 +0300

    feat: Add Main and scss

---
+++ src/components/App/App/App.tsx
@@ -0,0 +1,15 @@
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Main from "../Main/Main";
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
