f37c2bdf1ccda79ed3b0695a96ed20ee46364c00
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-21 11:40:30 +0300

    feat: Add desctop nav

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -1,5 +1,28 @@
 function Header() {
-  return <header></header>;
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="nav__menu nav__menu_type_left">
+          <a href="!#" className="nav__link  nav__text nav__link_more">
+            МАГАЗИН
+          </a>
+          <a href="!#" className="nav__link nav__text">
+            Доставка и оплата
+          </a>
+          <a href="!#" className="nav__link nav__text">
+            Адреса магазинов и контакты
+          </a>
+        </div>
+        <div className="nav__menu nav__menu_type_right">
+          <button className="nav__button  nav__text nav__button_type_enter">Войти</button>
+          <button className="nav__button nav__button_type_search" aria-label="Поиск"></button>
+          <button className="nav__button nav__button_type_favourite" aria-label="Избранное"></button>
+          <button className="nav__button nav__button_type_basket" aria-label="Корзина"><span className="nav__text">2</span></button>
+        </div>
+      </nav>
+      <section className="promo"></section>
+    </header>
+  );
 }
 
 export default Header;
