8026865d27f41cfafd41e3e8733b0f16ac9056bd
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-23 23:08:32 +0300

    feat: Add mobile Category

--- src/components/Main/Content/Content.tsx
+++ src/components/Main/Content/Content.tsx
@@ -1,10 +1,8 @@
 import { ContentCategoryProps } from "../../../utils/interfaces";
-import Showcase from "../Showcase/Showcase";
 
 function Content({ children, titleText, buttonText }: ContentCategoryProps) {
   return (
     <section className="content">
-      <Showcase />
       <div className="content__menu">
         <h2 className="content__title">{titleText}</h2>
         <button className="content__button content__button_position_top">
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,13 +1,28 @@
-import { CATEGORY_BUTTON_TEXT, CATEGORY_TITLE_TEXT, COLLECTION_BUTTON_TEXT, COLLECTION_TITLE_TEXT } from "../../utils/constants";
+import {
+  CATEGORY_BUTTON_TEXT,
+  CATEGORY_TITLE_TEXT,
+  COLLECTION_BUTTON_TEXT,
+  COLLECTION_TITLE_TEXT,
+} from "../../utils/constants";
 import Category from "./Category/Category";
 import Collection from "./Collection/Collection";
 import Content from "./Content/Content";
+import Showcase from "./Showcase/Showcase";
 
 function Main() {
   return (
     <main className="main">
-      <Content children={<Category />} titleText={CATEGORY_TITLE_TEXT} buttonText={CATEGORY_BUTTON_TEXT}/>
-      <Content children={<Collection />} titleText={COLLECTION_TITLE_TEXT} buttonText={COLLECTION_BUTTON_TEXT}/>
+      <Showcase />
+      <Content
+        children={<Category />}
+        titleText={CATEGORY_TITLE_TEXT}
+        buttonText={CATEGORY_BUTTON_TEXT}
+      />
+      <Content
+        children={<Collection />}
+        titleText={COLLECTION_TITLE_TEXT}
+        buttonText={COLLECTION_BUTTON_TEXT}
+      />
     </main>
   );
 }
