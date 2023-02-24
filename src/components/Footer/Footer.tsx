6985b72f707fe3f81f05b163abd7a63fbe9ac7ca
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-25 00:07:37 +0300

    feat: Add desctop Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,93 @@
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <ul className="footer__links">
+          <li>
+            <a href="!#" className="footer__link">
+              О нас
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Оптовикам
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Пресса
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Условия доставки
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Telegram
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Контакты
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Условия возврата
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Оплата
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Вакансии
+            </a>
+          </li>
+          <li>
+            <a href="!#" className="footer__link">
+              Vk
+            </a>
+          </li>
+        </ul>
+        <div className="footer__details">
+          <ul className="footer__langs">
+            <li>
+              <button className="footer__link footer__link_type_button">RU</button>
+            </li>
+            <li>
+              <button className="footer__link footer__link_type_button">EN</button>
+            </li>
+          </ul>
+          <ul className="footer__pays">
+            <li>
+              <button className="footer__pay footer__pay_type_mir" aria-label="Карта мир"></button>
+            </li>
+            <li>
+              <button className="footer__pay  footer__pay_type_visa" aria-label="Карта виза"></button>
+            </li>
+          </ul>
+          <a href="!#" className="footer__link">
+            alvde
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
