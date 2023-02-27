d09e2446eab767ce27e1adbdaae187b45db3028e
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-27 06:44:28 +0300

    feat: Add _rightHeaderMenu.scss

--- src/components/Header/RightHeaderMenu/RightHeaderMenu.tsx
+++ src/components/Header/RightHeaderMenu/RightHeaderMenu.tsx
@@ -1,22 +1,22 @@
 function RightHeaderMenu() {
   return (
-    <div className="nav__menu nav__menu_type_right">
-      <button className="nav__button  nav__text nav__button_type_enter">
+    <div className="icon-menu icon-menu_type_right">
+      <button className="icon-menu__button  icon-menu__text icon-menu__button_type_enter">
         Войти
       </button>
       <button
-        className="nav__button nav__button_type_search"
+        className="icon-menu__button icon-menu__button_type_search"
         aria-label="Поиск"
       ></button>
       <button
-        className="nav__button nav__button_type_favourite"
+        className="icon-menu__button icon-menu__button_type_favourite"
         aria-label="Избранное"
       ></button>
       <button
-        className="nav__button nav__button_type_basket"
+        className="icon-menu__button icon-menu__button_type_basket"
         aria-label="Корзина"
       >
-        <span className="nav__text">2</span>
+        <span className="icon-menu__text">2</span>
       </button>
     </div>
   );
