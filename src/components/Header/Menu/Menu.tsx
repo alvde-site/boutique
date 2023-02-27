153f7d56d3bfaf3c66766b368a7cf4deb776d731
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-27 19:40:21 +0300

    feat: Add _menu.scss

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -2,18 +2,18 @@ import { IHeaderProps } from "../../../utils/interfaces";
 
 function Menu(props: IHeaderProps) {
   return (
-    <div className="nav__menu nav__menu_type_left">
+    <div className="menu menu__type_left">
       <a
         href="!#"
-        className="nav__link  nav__text nav__link_more"
+        className="menu__link  menu__text menu__link_more"
         onClick={props.openMenu}
       >
         МАГАЗИН
       </a>
-      <a href="!#" className="nav__link nav__text">
+      <a href="!#" className="menu__link menu__text">
         Доставка и оплата
       </a>
-      <a href="!#" className="nav__link nav__text">
+      <a href="!#" className="menu__link menu__text">
         Адреса магазинов и контакты
       </a>
     </div>
