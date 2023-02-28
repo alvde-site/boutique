ea00c45d4f3e0756914aea5dd00ed1a04c91a7ce
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-28 22:40:26 +0300

    feat: Add contacts__data

--- src/components/Main/Contacts/Contacts.tsx
+++ src/components/Main/Contacts/Contacts.tsx
@@ -7,28 +7,52 @@ function Contacts() {
         <h2 className="contacts__title">Контакты</h2>
         <div className="contacts__info">
           <div className="contacts__adress">
-            <p className="contacts__city">Москва</p>
-            <h3 className="contacts__subtitle">Адрес студии:</h3>
+            <h3 className="contacts__city">Москва</h3>
+            <h4 className="contacts__subtitle">Адрес студии:</h4>
             <p className="contacts__item">Большая Садовая улица, 3с8</p>
-            <h3 className="contacts__subtitle">Режим работы:</h3>
+            <h4 className="contacts__subtitle">Режим работы:</h4>
             <p className="contacts__item">
               10:00 - 19:00 по предварительному звонку
             </p>
           </div>
           <div className="contacts__socials">
-            <p className="contacts__tel">+79268304044</p>
-            <ul className="contacts__icon">
+            <a className="contacts__tel" href="tel:+79268304044">+79268304044</a>
+            <ul className="contacts__icons">
               <li>
-                <button aria-label="Инстаграмм"></button>
+                <a
+                  href="!#"
+                  aria-label="Инстаграмм"
+                  className="contacts__icon contacts__icon_type_instagram"
+                >
+                  {" "}
+                </a>
               </li>
               <li>
-                <button aria-label="Вконтаке"></button>
+                <a
+                  href="!#"
+                  aria-label="Вконтаке"
+                  className="contacts__icon contacts__icon_type_vk"
+                >
+                  {" "}
+                </a>
               </li>
               <li>
-                <button aria-label="Телеграмм"></button>
+                <a
+                  href="!#"
+                  aria-label="Телеграмм"
+                  className="contacts__icon contacts__icon_type_tg"
+                >
+                  {" "}
+                </a>
               </li>
               <li>
-                <button aria-label="Фэйсбук"></button>
+                <a
+                  href="!#"
+                  aria-label="Фэйсбук"
+                  className="contacts__icon contacts__icon_type_facebook"
+                >
+                  {" "}
+                </a>
               </li>
             </ul>
           </div>
@@ -41,9 +65,9 @@ function Contacts() {
       <div className="contacts__cities">
         <h2 className="contacts__title">другие города</h2>
         <ul className="contacts__cards">
-          {cityContacts.map((data) => {
+          {cityContacts.map((data, i) => {
             return (
-              <li className="contacts__card card">
+              <li className="contacts__card card" key={i}>
                 <h3 className="card__city">{data.city}</h3>
                 <p className="card__label">{data.label}</p>
                 <p className="card__addres">{data.address}</p>
