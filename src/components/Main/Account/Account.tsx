cff8e5f8e669f2cc3f5ac7a9fe0b93e5ec421f36
Aleksandr Demidenko <sashatro@mail.ru>
2023-05-13 19:30:01 +0300

    feat: Add second order template

--- src/components/Main/Account/Account.tsx
+++ src/components/Main/Account/Account.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useAppSelector } from "../../../utils/hooks";
 import { selectAllInOrder } from "../../../services/reducers/productsSlice";
 import { useEffect, useState } from "react";
+import { product1 } from "../../../utils/constants";
 
 function Account() {
   const [totalPrice, setTotalPrice] = useState(0);
@@ -14,19 +15,13 @@ function Account() {
       setTotalPrice(() => 0);
     }
   }, [orderProducts]);
-  return (
-    <section className="account">
-      <div className="account__menu">
-        <Link className="account__signout-button" to="/">
-          Выйти
-        </Link>
-        <div className="account__buttons">
-          <button className="account__button">ЗАКАЗЫ</button>
-          <button className="account__button">ПРОФИЛЬ</button>
-        </div>
-      </div>
-      <div className="account__orders">
-        <div className="account__images order">
+
+  let order;
+
+  if (orderProducts.length) {
+    if (orderProducts.length) {
+      order = (
+        <div className="account__order order">
           <div className="order__images">
             {orderProducts.map((details) => (
               <img src={details.img} alt={details.title}></img>
@@ -43,29 +38,84 @@ function Account() {
                 <p className="order__text">{details.price} ₽</p>
               </div>
             ))}
-          </div>
-          <div className="order__field">
-            <p className="order__lable">комментарий:</p>
-            <p className="order__text">комментарий: Мирон жду аванс</p>
-          </div>
-          <div className="order__field">
-            <p className="order__lable">статус</p>
-            <p className="order__text">в обработке</p>
-          </div>
-          <div className="order__field">
-            <p className="order__lable">доставка</p>
-            <p className="order__text">курьером за пределы мкад (790 ₽)</p>
-          </div>
-          <div className="order__field">
-            <p className="order__lable">сумма</p>
-            <p className="order__text">{totalPrice} ₽</p>
-          </div>
-          <div className="order__field">
-            <p className="order__lable">трек номер</p>
-            <p className="order__text">DF43FJOAP009</p>
+            <div className="order__field">
+              <p className="order__lable">комментарий:</p>
+              <p className="order__text">Мирон жду аванс</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">статус</p>
+              <p className="order__text">в обработке</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">доставка</p>
+              <p className="order__text">курьером за пределы мкад (790 ₽)</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">сумма</p>
+              <p className="order__text">{totalPrice} ₽</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">трек номер</p>
+              <p className="order__text">DF43FJOAP009</p>
+            </div>
+            <button className="order__button">Отменить этот заказ</button>
           </div>
         </div>
-        <div className="account__order order"></div>
+      );
+    } else {
+      <></>;
+    }
+  }
+
+  return (
+    <section className="account">
+      <div className="account__menu">
+        <Link className="account__signout-button" to="/">
+          Выйти
+        </Link>
+        <div className="account__buttons">
+          <button className="account__button">ЗАКАЗЫ</button>
+          <button className="account__button">ПРОФИЛЬ</button>
+        </div>
+      </div>
+      <div className="account__orders">
+        {order}
+        <div className="account__order order">
+          <div className="order__images">
+            <img src={product1} alt="Denim effect pants"></img>
+          </div>
+          <div className="order__description">
+            <div className="order__field">
+              <p className="order__lable">№ 2712</p>
+              <p className="order__text">19.02.2022</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">Denim effect pants [x1]</p>
+              <p className="order__text">8000 ₽</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">комментарий:</p>
+              <p className="order__text">-</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">статус</p>
+              <p className="order__text">Доставлен</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">доставка</p>
+              <p className="order__text">курьер (490 ₽)</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">сумма</p>
+              <p className="order__text">8490 ₽</p>
+            </div>
+            <div className="order__field">
+              <p className="order__lable">трек номер</p>
+              <p className="order__text">DF43FJOAP009</p>
+            </div>
+            <button className="order__button">Отменить этот заказ</button>
+          </div>
+        </div>
       </div>
       <div className="account__profile"></div>
     </section>
